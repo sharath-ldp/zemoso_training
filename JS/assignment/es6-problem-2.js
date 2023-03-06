018f765fc65acf0e799cdc86cbc0f1a39ecf3afc
sharath-ldp <sharath.d@zemosolabs.com>
2023-03-06 11:41:28 +0530

    js asignment upto es6

---
+++ JS/assignment/es6-problem-2.js
@@ -0,0 +1,6 @@
+// const printBill = (name, bill) => {
+//     return “Hi “ + name + “, please pay: “ + bill;
+// }
+
+const printBill = (name, bill) => `Hi ${name}, please pay: ${bill}`;
+console.log(printBill("Sharath",148207))
